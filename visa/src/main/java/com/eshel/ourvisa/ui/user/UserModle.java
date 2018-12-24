1d0b77ad8988983980a782e67173a4e233062a5f
Eshel <shiwen.gsw@aliyun.com>
2018-12-24 18:24:15 +0800

    增加登录注册API

--- visa/src/main/java/com/eshel/ourvisa/ui/user/UserModle.java
+++ visa/src/main/java/com/eshel/ourvisa/ui/user/UserModle.java
@@ -1,11 +1,13 @@
 package com.eshel.ourvisa.ui.user;
 
+import com.eshel.ourvisa.bean.net.User;
 import com.eshel.ourvisa.mvp.base.Modle;
 import com.eshel.ourvisa.mvp.modles.IUserModle;
 import com.eshel.ourvisa.mvp.modles.modlecallback.IUserModleCallback;
 
 public class UserModle extends Modle<IUserModleCallback> implements IUserModle {
 
+    private User mUser;
     public UserModle(IUserModleCallback callback) {
         super(callback);
     }
